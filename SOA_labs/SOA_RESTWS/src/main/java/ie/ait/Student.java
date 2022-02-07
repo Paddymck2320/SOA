a1a80b4a43feb1f46a7675de714c0f5b2a08b4db
Paddymck2320 <56718903+Paddymck2320@users.noreply.github.com>
2022-02-07 15:50:17 +0000

    SOA_RESTWS

---
+++ SOA_labs/SOA_RESTWS/src/main/java/ie/ait/Student.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ie.ait;
+
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+@XmlRootElement(name = "student")
+@XmlType(propOrder = {"id", "name", "address", "course"}) 
+/**
+ *
+ * @author A00261481
+ */
+public class Student {
+    
+   private int id;
+   private String name; 
+   private String address; 
+   private String course;
+   
+   public Student(int id, String name, String address, String course){
+       this.id = id;
+       this.name = name;
+       this.address = address;
+       this.course =  course;
+   }
+   
+   public Student(){
+   }
+   
+   public int getId(){
+       return id;
+   }
+   
+   public void setId(int id){
+       this.id = id;
+   }
+   public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCourse() {
+        return course;
+    }
+
+    public void setCourse(String course) {
+        this.course = course;
+    }
+}
---
+++ SOA_labs/lab4/SOA_RESTWS/src/main/java/ie/ait/StudentResource.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ie.ait;
+
+import java.io.IOException;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import java.util.*;
+import javax.ws.rs.core.*;
+import javax.ws.rs.core.Context;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author a00266219
+ */
+
+@Path("students")
+public class StudentResource {
+   
+    @GET
+    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    public List<Student> getStudents() {
+        System.out.println("Hello.. Testing");
+        return StudentDao.instance.getStudents();
+    }
+    
+    @GET
+    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    @Path("{studentId}")
+    public Student getStudent(@PathParam("studentId") String id) {
+        return StudentDao.instance.getStudent(Integer.parseInt(id));
+    }
+    
+    @POST
+    @Produces({MediaType.TEXT_HTML})
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    public void postStudent(@FormParam("id") int id,
+            @FormParam("name") String name,
+            @FormParam("address") String address,
+            @FormParam("course") String course,
+            @Context HttpServletResponse servletResponse) throws IOException {
+        
+                   Student student = new Student();
+                   student.setId(id);
+                   student.setCourse(course);
+                   student.setName(name);
+                   student.setAddress(address);
+                   StudentDao.instance.create(student);
+                   servletResponse.sendRedirect("../index.html");
+            }
+    
+    @DELETE
+    @Produces(MediaType.TEXT_HTML)
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    @Path("{studentId}")
+    public Response deleteStudent(@PathParam("studentId") String id) {
+        
+        StudentDao.instance.delete(Integer.parseInt(id));
+        return Response.status(200).entity("Operation Successful").build();
+    }
+    
+}
